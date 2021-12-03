c42107e9d9b649a840615e80b2014dd01b817522
Diego Adilson Vieira <diegoavieira@gmail.com>
2021-12-03 17:50:20 -0300

    fix: adjustments

--- libs/components/RdsLoading/RdsLoading.styles.ts
+++ libs/components/RdsLoading/RdsLoading.styles.ts
@@ -1,9 +1,10 @@
-import { createStyles, Theme } from '@material-ui/core';
+import { alpha, createStyles, Theme } from '@material-ui/core';
 
 const RdsLoadingStyles = (theme: Theme) =>
   createStyles({
     root: {
-      zIndex: theme.zIndex.drawer + 1
+      zIndex: theme.zIndex.drawer + 1,
+      backgroundColor: alpha(theme.palette.common.white, 0.5)
     }
   });
 
